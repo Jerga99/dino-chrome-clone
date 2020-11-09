2e62c20a06b126b1a6128c56a60d5603b49daccd
Filip Jerga <jerga99@gmail.com>
2020-11-09 01:51:05 +0100

    Initial commit

---
+++ src/PlayScene.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser';
+
+class PlayScene extends Phaser.Scene {
+
+  constructor() {
+    super('PlayScene');
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+}
+
+export default PlayScene;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import Phaser from 'phaser';
+
+import PlayScene from './PlayScene';
+import PreloadScene from './PreloadScene';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 340,
+  pixelArt: true,
+  transparent: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  scene: [PreloadScene, PlayScene]
+};
+
+new Phaser.Game(config);
