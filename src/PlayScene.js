49f1f941966ef151035d65d6cf459a038af2bcd3
Filip Jerga <jerga99@gmail.com>
2020-11-09 11:01:21 +0100

    Create controll

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -11,7 +11,20 @@ class PlayScene extends Phaser.Scene {
     this.gameSpeed = 10;
 
     this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1)
-    this.dino = this.physics.add.sprite(0, height, 'dino-idle').setOrigin(0, 1);
+    this.dino = this.physics.add.sprite(0, height, 'dino-idle')
+      .setCollideWorldBounds(true)
+      .setGravityY(5000)
+      .setOrigin(0, 1);
+
+    this.createControll();
+  }
+
+  createControll() {
+    this.input.keyboard.on('keydown_SPACE', () => {
+      if (!this.dino.body.onFloor()) { return; }
+
+      this.dino.setVelocityY(-1600);
+    })
   }
 
   update() {
