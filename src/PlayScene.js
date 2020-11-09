b37343f91e82c4409552fe346c406bfd8c2096bd
Filip Jerga <jerga99@gmail.com>
2020-11-09 10:55:35 +0100

    Adds dino and ground

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -7,11 +7,15 @@ class PlayScene extends Phaser.Scene {
   }
 
   create() {
+    const { height, width } = this.game.config;
+    this.gameSpeed = 10;
 
+    this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1)
+    this.dino = this.physics.add.sprite(0, height, 'dino-idle').setOrigin(0, 1);
   }
 
   update() {
-
+    this.ground.tilePositionX += this.gameSpeed;
   }
 }
 
